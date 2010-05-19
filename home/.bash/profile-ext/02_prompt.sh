ca8a32922ddd7994dbef6482214de94a701f7177
Darcy Laycock <sutto@sutto.net>
2010-05-19 20:45:08 +0800

    Add prompt colour fixes

--- home/.bash/profile-ext/02_prompt.sh
+++ home/.bash/profile-ext/02_prompt.sh
@@ -1,53 +1,62 @@
-GIT_PS1_SHOWDIRTYSTATE=1
+git_dirty_marker="♼ "
 
-function __pc {
+__pc() {
   ~/.rvm/scripts/color "$1"
 }
 
-function __bash_git_branch {
-  local git_dir=$(git rev-parse --git-dir 2>/dev/null)
-  [[ -z "$git_dir" ]] && return 0
-  
-  local git_branch=$(git symbolic-ref HEAD 2>/dev/null| cut -d / -f 3)
-  
-  if [[ -z "$git_branch" ]]; then
-    local git_branch=$(git log HEAD^..HEAD --pretty=format:%h | head -1 2>/dev/null)
-  fi
-  
-  echo -n "$git_branch "
-}
-
-function __bash_git_dirty {
-  local git_dir=$(git rev-parse --git-dir 2>/dev/null)
-  [[ -z "$git_dir" ]] && return 0
-  
-  if [[ ! -z $(git status | grep 'added to commit' 2> /dev/null) ]]; then
-    echo -n "♼ "  
-  fi 
-
-}
-
-function __bash_pwd_prompt_additions {
-  if [[ "$PWD" = ~ ]]; then
-    echo -n '~'
+_prompt_colour() {
+  echo -n '\['
+  if [[ "$1" -eq "default" ]]; then
+    echo -e -n "\033[0m"
   else
-    basename "$PWD"
+    local color_number
+    case "$1" in
+    green)
+    color_number=2;
+    ;;
+    blue)
+    color_number=4;
+    ;;
+    magenta)
+    color_number=5;
+    ;;
+    yellow)
+    color_number=3;
+    ;;
+    *)
+    color_number=9;
+    ;;
+    esac
+    echo -n -e "\033[3${color_number}m"
+  fi
+  echo -n '\]'
+}
+
+__bash_git_branch() {
+  local git_branch=$(git symbolic-ref HEAD 2> /dev/null| cut -d / -f 3)
+  if [ -z "$git_branch" ]; then
+    git_branch=$(git rev-parse HEAD 2> /dev/null | cut -c6)
+  fi
+  [[ -n "$git_branch" ]] && echo -n "on $git_branch "
+}
+
+__bash_git_dirty() {
+  if [[ -n $(git status -s 2> /dev/null) ]]; then
+    echo -n "$git_dirty_marker "
   fi
 }
 
-function __bash_rvm_prompt_additions {
-  if [[ -r ~/.rvm/bin/rvm-prompt ]]; then
-    local interpreter=$(~/.rvm/bin/rvm-prompt u)
-    if [[ ! -z "$interpreter" ]]; then
-      echo -n "$interpreter "
-    fi
+__bash_rvm_prompt_additions() {
+  if [[ -x ~/.rvm/bin/rvm-prompt ]]; then
+    local interpreter=$(~/.rvm/bin/rvm-prompt)
+    [[ -n "$interpreter" ]] && echo -n "using $interpreter "
   fi
 }
 
 # Each part of the prompt.
-_prompt_pwd="\[\$(__pc green)\]\$(__bash_pwd_prompt_additions)\[\$(__pc default)\]"
+_prompt_pwd="\$(_prompt_color green)\W\[\$(__pc default)\]"
 _prompt_git_branch="\[\$(__pc blue)\]\$(__bash_git_branch)\[\$(__pc default)\]"
 _prompt_git_dirty="\[\$(__pc magenta)\]\$(__bash_git_dirty)\[\$(__pc default)\]"
 _prompt_rvm_interpreter="\[\$(__pc yellow)\]\$(__bash_rvm_prompt_additions)\[\$(__pc default)\]"
 
-PS1="${_prompt_rvm_interpreter}${_prompt_pwd} ${_prompt_git_branch}${_prompt_git_dirty}\\$ "
+PS1="${_prompt_pwd} ${_prompt_git_branch}${_prompt_rvm_interpreter}${_prompt_git_dirty}\n\\$ "
