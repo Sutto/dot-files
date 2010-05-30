f427a1f9695909db306e6cb612996895cec0e589
Darcy Laycock <sutto@sutto.net>
2010-05-31 05:50:53 +0800

    Update completions

--- home/.bash/profile-ext/00_completions.sh
+++ home/.bash/profile-ext/00_completions.sh
@@ -1,5 +1,6 @@
 # Import default completions
-bash_completions="/Users/sutto/.rvm/scripts/completion /usr/local/Library/Contributions/brew_bash_completion.sh /usr/local/etc/bash_completion.d/* ~/.bash/completions/*.sh"
-for completion in $bash_completions; do
-  [[ -r "$completion" ]] && source $completion
+bash_completions=(~/.rvm/scripts/completion /usr/local/Library/Contributions/brew_bash_completion.sh /usr/local/etc/bash_completion.d/* ~/.bash/profile-ext/completions/*.sh)
+for completion in ${bash_completions[*]}; do
+  [[ -r $completion ]] && source $completion
 done
+
